80a3c9db62342a47feb0fcd9f79364b21cb44562
wflixu <wflixu@88.com>
2024-01-06 23:46:24 +0800

    v0.0.3

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+import pkg from './../../package.json'
+const version = computed(()=>{
+  return pkg.version
+})
+
+const count = ref(0)
+
+const onDecrease = () =>{
+  count.value -= 1;
+}
+const onIncrease = () =>{
+  count.value +=1;
+}
+</script>
 
 <template>
   <main>
     <h1>Thinking</h1>
+    <h3>版本：{{version}}</h3>
+    <div>
+      <h2> counter</h2>
+      <button @click="onIncrease" > 加 + </button><span>{{count}}</span><button @click="onDecrease"> 减 -</button>
+    </div>
   </main>
 </template>
