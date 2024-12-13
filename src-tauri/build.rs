813a1b16df6b98c58698edf4dc6c1e95fd2a5f6e
wflixu <wflixu@88.com>
2024-12-13 11:21:06 +0800

    chore:updater

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,9 @@
+use std::env;
+
 fn main() {
+    // Set environment variables here
+    env::set_var("TAURI_SIGNING_PRIVATE_KEY", "/Users/lixu/.tauri/thinking.key");
+    env::set_var("TAURI_SIGNING_PRIVATE_KEY_PASSWORD", "");
+    
     tauri_build::build()
 }
