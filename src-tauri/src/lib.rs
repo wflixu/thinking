a7269775fa1a525e3507309928ebdc021005b0a4
wflixu <wflixu@88.com>
2024-12-13 14:34:31 +0800

    chore:v0.2.1

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -8,8 +8,12 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        // .plugin(tauri_plugin_updater::Builder::new().build())
         .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            #[cfg(desktop)]
+            app.handle().plugin(tauri_plugin_updater::Builder::new().build());
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
